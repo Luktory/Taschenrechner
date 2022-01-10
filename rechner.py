00d0b2701639f64ea8fd20aafb9916f2ffac16d9
Luktory <88310218+Luktory@users.noreply.github.com>
2022-01-10 14:09:21 +0100

    Add files via upload
    Der Code

---
+++ rechner.py
@@ -0,0 +1,20 @@
+print("Das ist ein sehr einfacher Taschenrechner")
+print("Dieser Taschenrechner kann +,-,* oder /")
+rechenop=input("Hier eingeben ob du +.-.* oder / rechnen möchtest:  ")
+zahl1=input("Die erste Zahl: ")
+zahl2=input("Die zweite Zahl: ")
+if rechenop == "+":
+    ergebnis=int(zahl1)+int(zahl2)
+    print("Das Ergebnis ist:" , ergebnis)
+
+if rechenop == "-":
+    ergebnis=int(zahl1)-int(zahl2)
+    print("Das Ergebnis ist:" , ergebnis)
+
+if rechenop == "*":
+    ergebnis=int(zahl1)*int(zahl2)
+    print("Das Ergebnis ist:" , ergebnis)
+
+if rechenop == "/":
+    ergebnis=int(zahl1)/int(zahl2)
+    print("Das Ergebnis ist:" , ergebnis)
